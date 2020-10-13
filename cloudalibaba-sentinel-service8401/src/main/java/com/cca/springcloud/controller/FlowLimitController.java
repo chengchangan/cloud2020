4ccbeadfc0dd88bb0c0ba1b4169315586fbb9ec9
冯克雷 <changan.cheng@greatonce.com>
2020-10-14 07:59:06 +0800

    新增 8401服务 被sentinel监控，TODO 熟悉熔断限流规则配置，以及自定义

--- cloudalibaba-sentinel-service8401/src/main/java/com/cca/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/cca/springcloud/controller/FlowLimitController.java
@@ -1,19 +1,24 @@
 package com.cca.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
+    private Integer count = 0;
 
     @GetMapping("/testA")
-    public String testA(){
-        return "-----testA";
+    public String testA() throws InterruptedException {
+        Thread.sleep(5000);
+        log.info("-------请求次数: {}, 线程名称: {}", ++count, Thread.currentThread().getName());
+        return "-----testA ,次数" + count;
     }
 
     @GetMapping("/testB")
-    public String testB(){
+    public String testB() {
         return "-----testB";
     }
 }
